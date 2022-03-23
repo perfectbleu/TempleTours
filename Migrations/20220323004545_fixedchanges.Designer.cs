0dc8c2c87939b674703d3747935041ec096dcfe7
dpjensen22 <dpjensen22@gmail.com>
2022-03-22 18:48:16 -0600

    migrations

--- Migrations/20220323004545_fixedchanges.Designer.cs
+++ Migrations/20220323004545_fixedchanges.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TempleTours.Models;
 
 namespace TempleTours.Migrations
 {
     [DbContext(typeof(ApptContext))]
-    partial class ApptContextModelSnapshot : ModelSnapshot
+    [Migration("20220323004545_fixedchanges")]
+    partial class fixedchanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
